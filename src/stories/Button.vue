6e27d2e271ec3e3df594642b190cbe1c707a21ac
Riaan Laubscher <riaan@platinumseed.com>
2024-06-13 16:20:49 +0200

    new button styles

--- src/stories/Button.vue
+++ src/stories/Button.vue
@@ -1,48 +1,66 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }} </button>
+	<button 
+		type="button" 
+		class="
+			group
+			inline-flex 
+			justify-center 
+			items-center 
+			gap-2 
+			px-5 
+			py-3
+			transition-all
+		" 
+		:class="buttonClasses"
+		@click="onClick" 
+		:style="style">
+		<span class="transition-all" :class="textClasses">{{ label }} </span>
+	</button>
 </template>
 
 <script lang="ts" setup>
-import './button.css';
 import { computed } from 'vue';
 
 const props = withDefaults(defineProps<{
-  /**
-   * The label of the button
-   */
-  label: string,
-  /**
-   * primary or secondary button
-   */
-  primary?: boolean,
-  /**
-   * size of the button
-   */
-  size?: 'small' | 'medium' | 'large',
-  /**
-   * background color of the button
-   */
-  backgroundColor?: string,
+	/**
+	 * The label of the button
+	 */
+	label: string,
+	/**
+	 * type of the button
+	 */
+	type?: 'primary' | 'secondary' | 'outline' | 'ghost',
+	/**
+	 * background color of the button
+	 */
+	backgroundColor?: string,
 
-}>(), { primary: false });
+}>(), { type: 'primary' });
 
 const emit = defineEmits<{
-  (e: 'click', id: number): void;
+	(e: 'click', id: number): void;
 }>();
 
-const classes = computed(() => ({
-  'storybook-button': true,
-  'storybook-button--primary': props.primary,
-  'storybook-button--secondary': !props.primary,
-  [`storybook-button--${props.size || 'medium'}`]: true,
+const buttonClasses = computed(() => ({
+	'rounded-full border': props.type == 'primary' || props.type == 'secondary' || props.type == 'outline',
+	'bg-orange border-orange hover:bg-amber-600 hover:border-amber-600': props.type == 'primary',
+	'bg-blue border-blue hover:border-sky-600 hover:bg-sky-600': props.type == 'secondary',
+	'bg-white border-blue hover:border-sky-600': props.type == 'outline',
+	'': props.type == 'ghost',
+}));
+
+const textClasses = computed(() => ({
+	'text-white': props.type == 'primary' || props.type == 'secondary',
+	'text-blue group-hover:text-sky-600': props.type == 'outline',
+	'text-digital-black hover:text-blue': props.type == 'ghost',
 }));
 
 const style = computed(() => ({
-  backgroundColor: props.backgroundColor
+	backgroundColor: props.backgroundColor
 }));
 
 const onClick = () => {
-  emit("click", 1)
+	emit("click", 1)
 };
 
 </script>

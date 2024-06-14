a452d165e1590f494539e654431fd2b1ad3e2ff6
Riaan Laubscher <riaan@platinumseed.com>
2024-06-14 08:19:58 +0200

    updated button catagory

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -4,19 +4,19 @@ import Button from './Button.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
-  title: 'Example/Button',
-  component: Button,
-  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  argTypes: {
-    type: { control: 'select', options: ['primary', 'secondary', 'outline', 'ghost'] },
-    backgroundColor: { control: 'color' },
-  },
-  args: {
-    type: 'primary',
-    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-    onClick: fn(),
-  },
+	title: 'Base/Button',
+	component: Button,
+	// This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+	tags: ['autodocs'],
+	argTypes: {
+		type: { control: 'select', options: ['primary', 'secondary', 'outline', 'ghost'] },
+		backgroundColor: { control: 'color' },
+	},
+	args: {
+		type: 'primary',
+		// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+		onClick: fn(),
+	},
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -27,29 +27,29 @@ type Story = StoryObj<typeof meta>;
  * to learn how to use render functions.
  */
 export const Primary: Story = {
-  args: {
-    type: 'primary',
-    label: 'Button',
-  },
+	args: {
+		type: 'primary',
+		label: 'Button',
+	},
 };
 
 export const Secondary: Story = {
-  args: {
-    type: 'secondary',
-    label: 'Button',
-  },
+	args: {
+		type: 'secondary',
+		label: 'Button',
+	},
 };
 
 export const Outline: Story = {
-  args: {
-    type: 'outline',
-    label: 'Button',
-  },
+	args: {
+		type: 'outline',
+		label: 'Button',
+	},
 };
 
 export const Ghost: Story = {
-  args: {
-    type: 'ghost',
-    label: 'Button',
-  },
+	args: {
+		type: 'ghost',
+		label: 'Button',
+	},
 };

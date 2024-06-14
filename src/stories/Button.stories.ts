77183cb714c6128d493ac5c62f5bb80a336a6386
Riaan Laubscher <riaan@platinumseed.com>
2024-06-14 15:22:12 +0200

    fixed conflict

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -4,19 +4,21 @@ import Button from './Button.vue';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
-  title: 'Example/Button',
-  component: Button,
-  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  argTypes: {
-    type: { control: 'select', options: ['primary', 'secondary', 'outline', 'ghost'] },
-    backgroundColor: { control: 'color' },
-  },
-  args: {
-    type: 'primary',
-    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-    onClick: fn(),
-  },
+	title: 'Base/Button',
+	component: Button,
+	// This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
+	tags: ['autodocs'],
+	argTypes: {
+		type: { control: 'select', options: ['primary', 'secondary', 'outline', 'ghost'] },
+		iconBefore: { control: 'select', options: ['arrow_back', 'check_circle'] },
+		iconAfter: { control: 'select', options: ['arrow_forward', 'check_circle'] },
+		disabled: { control: 'boolean' },
+	},
+	args: {
+		type: 'primary',
+		// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+		onClick: fn(),
+	},
 } satisfies Meta<typeof Button>;
 
 export default meta;

56e719b3edbfededd48e142280b4912bd8fdfd79
Riaan Laubscher <riaan@platinumseed.com>
2024-06-26 09:04:23 +0200

    import material fonts from external url

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,9 +15,6 @@ export default defineConfig({
 		cssCodeSplit: false, // Combine all CSS into one file
 		outDir: 'dist',
 		rollupOptions: {
-			input: {
-				main: resolve(__dirname, 'src/index.ts'),
-			},
 			output: {
 				assetFileNames: 'styles/[name].[ext]', // Customize output path
 			},

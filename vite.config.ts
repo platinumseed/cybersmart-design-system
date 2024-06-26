2f8f02f09d8f8b560abdc5caa99e36e9d86e9f98
Riaan Laubscher <riaan@platinumseed.com>
2024-06-26 08:32:25 +0200

    relative path for font file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,9 @@ export default defineConfig({
 		cssCodeSplit: false, // Combine all CSS into one file
 		outDir: 'dist',
 		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'src/index.ts'),
+			},
 			output: {
 				assetFileNames: 'styles/[name].[ext]', // Customize output path
 			},

268ee04899a7cf780d5a5204ca0ae633c658569b
Riaan Laubscher <riaan@platinumseed.com>
2024-07-11 12:37:15 +0200

    made search input fill width

--- src/stories/SearchInput/SearchInput.vue
+++ src/stories/SearchInput/SearchInput.vue
@@ -2,10 +2,10 @@
 	<div class="relative">
 		<div class="material-symbols-outlined absolute left-4 top-[10px] text-stone-800 text-base" aria-hidden="true">search</div>
 		<input
-			class="ps-10 pe-4 py-2 bg-white rounded-xl border transition-all focus:border-sky-500 focus-visible:outline-none"
+			v-bind="$attrs" 
+			class="ps-10 pe-4 py-2 bg-white rounded-xl border transition-all focus:border-sky-500 focus-visible:outline-none w-full"
 			:class="inputClass"
 			type="search"
-			v-bind="$attrs" 
 			:name="name" 
 			:id="name" 
 			:value="internalValue"

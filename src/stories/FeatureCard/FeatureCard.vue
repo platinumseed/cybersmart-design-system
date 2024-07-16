daa6a1d027d6f2205f91fa31d09d48c95d5a62b6
Riaan Laubscher <riaan@platinumseed.com>
2024-07-16 12:30:56 +0200

    styling fix

--- src/stories/FeatureCard/FeatureCard.vue
+++ src/stories/FeatureCard/FeatureCard.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="flex flex-col gap-6 items-center">
 		<div class="w-12 h-12 p-2 bg-white rounded-xl border border-slate-300 justify-center items-center gap-2 inline-flex material-symbols-outlined">{{ icon }}</div>
-		<div class="text-xl lg:text-2xl 2xl:text-2xl text-primary text=text-center">{{ title }}</div>
-		<p class="text-secondary text=text-center">{{ description }}</p>
+		<div class="text-xl lg:text-2xl 2xl:text-2xl text-primary text-center">{{ title }}</div>
+		<p class="text-secondary text-center">{{ description }}</p>
 		<Button
 			v-if="linkUrl && linkUrl"
 			type="ghost"

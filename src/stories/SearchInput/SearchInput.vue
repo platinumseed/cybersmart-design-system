002d19fbfe9db91c19b9581db0846e456502ffe3
Riaan Laubscher <riaan@platinumseed.com>
2024-07-17 11:51:38 +0200

    added ref to search input

--- src/stories/SearchInput/SearchInput.vue
+++ src/stories/SearchInput/SearchInput.vue
@@ -3,6 +3,7 @@
 		<div class="material-symbols-outlined absolute left-4 top-[10px] text-stone-800 text-base" aria-hidden="true">search</div>
 		<input
 			v-bind="$attrs" 
+			ref="input"
 			class="ps-10 pe-4 py-2 bg-white rounded-xl border transition-all focus:border-sky-500 focus-visible:outline-none w-full"
 			:class="inputClass"
 			type="search"

811901bf22534cf278c630c890dd3f9f4d8d3752
Riaan Laubscher <riaan@platinumseed.com>
2024-07-29 10:59:31 +0200

    added new formatting to FeatureCard

--- src/stories/FeatureCard/FeatureCard.vue
+++ src/stories/FeatureCard/FeatureCard.vue
@@ -1,15 +1,21 @@
 <template>
-	<div class="flex flex-col gap-6 items-center">
-		<div class="w-12 h-12 p-2 bg-white rounded-xl border border-slate-300 justify-center items-center gap-2 inline-flex material-symbols-outlined">{{ icon }}</div>
-		<div class="text-xl lg:text-2xl 2xl:text-2xl text-primary text-center">{{ title }}</div>
-		<p class="text-secondary text-center">{{ description }}</p>
-		<Button
-			v-if="linkUrl && linkUrl"
-			type="ghost"
-			:label="linkText"
-			:url="linkUrl"
-			icon-after="arrow_right_alt"
-		/>
+	<div class="flex flex-col gap-6" :class="{'items-center': textAlign === 'center' }">
+		<div 
+			class="material-symbols-outlined"
+			:class="iconStyle === 'boxed' ? 'w-12 h-12 bg-white rounded-xl border border-slate-300 justify-center items-center gap-2 inline-flex p-2' : 'text-blue'"
+		>{{ icon }}</div>
+		<div class="text-xl lg:text-2xl 2xl:text-2xl text-primary" :class="{'text-center': textAlign === 'center' }">{{ title }}</div>
+		<p class="text-secondary" :class="{'text-center': textAlign === 'center' }">{{ description }}</p>
+		<div>
+			<Button
+				v-if="linkUrl && linkUrl"
+				type="ghost"
+				:label="linkText"
+				:url="linkUrl"
+				icon-after="arrow_right_alt"
+				:class="'!p-0'"
+			/>
+		</div>
 	</div>
 </template>
 
@@ -20,9 +26,15 @@ interface Props {
 	title: string;
 	description: string;
 	icon: string;
+	iconStyle: 'boxed' | 'open'
 	linkText?: string;
 	linkUrl?: string;
+	textAlign?: 'left' | 'center';
 }
 
-defineProps<Props>();
+withDefaults(defineProps<Props>(), {
+	iconStyle: 'boxed',
+	textAlign: 'center',
+});
+
 </script>

f08c2f2f2815e88e20827c354429cdfd8d0f2c19
Riaan Laubscher <riaan@platinumseed.com>
2024-08-15 10:08:02 +0200

    updated feature list to allow for 2 columns

--- src/stories/FeatureList/FeatureList.vue
+++ src/stories/FeatureList/FeatureList.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="flex flex-col gap-4">
-		<div v-for="featureItem in featureItems" :key="featureItem" class="flex items-center gap-2">
+	<div :class="colClass">
+		<div v-for="featureItem in featureItems" :key="featureItem" class="flex items-center gap-2 mb-4 last:mb-0'">
 			<span :class="`${iconColor} material-symbols-outlined transition-all`">check_circle</span>
 			<span :class="`${textColor} text-lg font-light transition-all`">{{ featureItem }}</span>
 		</div>
@@ -8,16 +8,25 @@
 </template>
 
 <script lang="ts" setup>
-
+import { computed } from 'vue'
 interface Props {
 	featureItems: string[]
 	iconColor?: string
 	textColor?: string
+	columns?: boolean
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
 	iconColor: 'text-blue',
 	textColor: 'text-slate-500',
+	columns: false
 });
+
+const colClass = computed(() => {
+	if (props.columns) {
+		return 'columns-2 gap-4'
+	}
+	return 'flex flex-col'
+})
 
 </script>
